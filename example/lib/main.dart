3c4ed5e057965453cb101438b82c99c491389a6a
Mohamed Toman <mtoman1985@gmail.com>
2023-05-03 08:44:57 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TableCalendar Example',
+      title: 'dfgfdgd Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
